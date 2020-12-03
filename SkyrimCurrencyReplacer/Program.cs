b870c41f79e4ae234ad88f1a2cc53d50fd481969
Deigue <deigue@hotmail.com>
2020-12-02 22:19:48 -0500

    Updated solution config, and initialized patcher project.

---
+++ SkyrimCurrencyReplacer/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+
+namespace SkyrimCurrencyReplacer
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
+                args: args,
+                patcher: RunPatch,
+                userPreferences: new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    {
+                        IdentifyingModKey = "YourPatcher.esp",
+                        TargetRelease = GameRelease.SkyrimSE,
+                        BlockAutomaticExit = true,
+                    }
+                });
+        }
+
+        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            //Your code here!
+        }
+    }
+}
