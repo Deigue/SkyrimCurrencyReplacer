f14fcaeae995d7945884065980764c2c4f3b1a8e
Deigue <deigue@hotmail.com>
2020-12-02 22:31:05 -0500

    Built base-plate / flowchart of implementation and trial/errors.

--- SkyrimCurrencyReplacer/CurrencyReplacer.cs
+++ SkyrimCurrencyReplacer/CurrencyReplacer.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 
 namespace SkyrimCurrencyReplacer
 {
-    public class Program
+    public static class CurrencyReplacer
     {
         public static int Main(string[] args)
         {
@@ -18,16 +15,24 @@ namespace SkyrimCurrencyReplacer
                 {
                     ActionsForEmptyArgs = new RunDefaultPatcher()
                     {
-                        IdentifyingModKey = "YourPatcher.esp",
+                        IdentifyingModKey = "SkyrimCurrencyReplacer.esp",
                         TargetRelease = GameRelease.SkyrimSE,
                         BlockAutomaticExit = true,
                     }
                 });
         }
 
-        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        private static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            // Detect presence of Replacer plugin.
+            
+            // Patch up Containers originating from CoT V2 Replacer.
+            
+            // Patch REFRs with randomized generated variations of coins/currencies across worldspaces and cells.
+            
+            // Handle leveled lists containing LVLI.
+            
+            // Handle flora activatable containers like pouches to emit currencies.
         }
     }
 }
